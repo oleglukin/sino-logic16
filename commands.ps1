11bf7a329df490524040c91c34a676d60686f413
Lukin <Oleg.Lukin@shell.com>
2019-09-08 00:15:24 +1000

    Updated docker compose file

--- commands.ps1
+++ commands.ps1
@@ -2,3 +2,13 @@
 dotnet publish --framework netcoreapp2.2 --configuration Release --output dist
 
 docker-compose build
+docker-compose up -d
+
+docker stop $(docker ps -q)
+docker rm $(docker ps -a)
+
+docker exec -it sino-logic16_webui_1 pwd
+
+# Map port 3000 to container's port 80
+docker run -p 3000:80 --rm --name sino-logic16_webui_1 sino-logic16_webui
+# http://localhost:3000/
