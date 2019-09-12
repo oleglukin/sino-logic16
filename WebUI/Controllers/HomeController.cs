2974a6d8bce50a6c763ace3ab0ef86415b25ae0e
Oleg Lukin <oleglukin@yahoo.com>
2019-09-12 23:37:14 +1000

    Index/Home view
    Added model output

--- WebUI/Controllers/HomeController.cs
+++ WebUI/Controllers/HomeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using WebUI.Models;
@@ -14,14 +15,15 @@ namespace WebUI.Controllers
         public HomeController(IConfiguration config)
         {
             message = config["MESSAGE"] ?? "Default message here";
-            jobs = new List<JobModel>();;
-           
+            jobs = new List<JobModel>();
+            jobs.Add(new JobModel{Id = "1", JobType = "Job A", Started = new DateTime(2019, 9, 12, 8, 48, 26)});
+            jobs.Add(new JobModel{Id = "2", JobType = "Job B", Started = DateTime.Now});
         }
 
         public IActionResult Index()
         {
             ViewBag.Message = message;
-            return View();
+            return View(jobs);
         }
 
         public IActionResult Privacy()
