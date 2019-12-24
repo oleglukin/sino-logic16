f8c0bbab3d9ea31150882064401410309a11de3a
Oleg Lukin <oleglukin@yahoo.com>
2019-12-25 00:21:07 +1000

    Added empty solution for .net

---
+++ net/sino-logic16-net-console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace sino_logic16_net_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
