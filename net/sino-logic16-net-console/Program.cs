e4d48b10f6cd2ab393e15588ae7f20de79d22bae
Oleg Lukin <oleglukin@yahoo.com>
2019-12-26 00:49:51 +1000

    Added sample console app with event processing

--- net/sino-logic16-net-console/Program.cs
+++ net/sino-logic16-net-console/Program.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using com.espertech.esper.client;
+using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace sino_logic16_net_console
 {
@@ -6,7 +9,30 @@ namespace sino_logic16_net_console
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Starting...");
+
+            EPServiceProvider engine = EPServiceProviderManager.GetDefaultProvider();
+            engine.EPAdministrator.Configuration.AddEventType<SignalEvent>();
+
+            string epl = "select Endpoint, Id from SignalEvent";
+            EPStatement statement = engine.EPAdministrator.CreateEPL(epl);
+
+            statement.Events += DefaultUpdateEventHandler;
+
+            engine.EPRuntime.SendEvent(new SignalEvent("Signal 42", 21));
+
+            Console.WriteLine("Finished. Press Enter...");
+            Console.ReadLine();
+        }
+
+        private static void DefaultUpdateEventHandler(object sender, UpdateEventArgs e)
+        {
+            var attributes = (e.NewEvents.FirstOrDefault().Underlying as Dictionary<string, object>);
+            Console.WriteLine("An Event (" + e.Statement.Name + ") occured:");
+            foreach(var att in attributes)
+            {
+                Console.WriteLine($"\t{att.Key}: {att.Value}");
+            }
         }
     }
 }
---
+++ net/sino-logic16-net-console/SignalEvent.cs
@@ -0,0 +1,14 @@
+﻿namespace sino_logic16_net_console
+{
+    class SignalEvent
+    {
+        public string Endpoint { get; set; }
+        public int Id { get; set; }
+
+        public SignalEvent(string endpoint, int id)
+        {
+            Endpoint = endpoint;
+            Id = id;
+        }
+    }
+}
