933756d36e1e46201331361a79395859a0cf6535
Oleg Lukin <oleglukin@yahoo.com>
2019-12-27 23:12:10 +1000

    Added WebAPI simple project

---
+++ net/Web/Controllers/SignalEventController.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using API.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class SignalEventController : ControllerBase
+    {
+        private readonly ILogger<SignalEventController> _logger;
+
+        public SignalEventController(ILogger<SignalEventController> logger)
+        {
+            _logger = logger;
+        }
+
+
+        [HttpPost]
+        public void Post([FromBody] SignalEvent signalEvent)
+        {
+            Console.WriteLine(signalEvent);
+            //_context.TodoItems.Add(todoItem);
+            //await _context.SaveChangesAsync(); // TODO do not await
+        }
+
+
+        [HttpGet]
+        public string Get()
+        {
+
+            return "SignalEventController GET placeholda";
+        }
+    }
+}
---
+++ net/Web/Models/SignalEvent.cs
@@ -0,0 +1,13 @@
+﻿namespace API.Models
+{
+    public class SignalEvent
+    {
+        // TODO follow naming conventions
+        public string id_sample { get; set; } // идетнификатор предмета(76rtw)
+        public string num_id { get; set; } // серийный номер предмета(ffg#er111)
+        public string id_location { get; set; } // место откуда(3211.2334), а может быть(Екатеринбург)
+        public string id_signal_par { get; set; } // датчик, с которого идет сигнал(0xcv11cs)
+        public string id_detected { get; set; } // данные о состоянии(None), - исправен, (Nan), - поломка
+        public string id_class_det { get; set; } // вид поломки(req11)
+    }
+}
