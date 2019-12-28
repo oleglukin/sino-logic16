94dc551bdd409367007935005b70b57d65fb3043
Oleg Lukin <oleglukin@yahoo.com>
2019-12-28 23:22:46 +1000

    Removed redundant code

--- net/API/Startup.cs
+++ net/API/Startup.cs
@@ -28,24 +28,11 @@ namespace Api
         {
             var engine = EPServiceProviderManager.GetDefaultProvider();
             engine.EPAdministrator.Configuration.AddEventType<SignalEvent>();
-            //string epl = "select id_sample, num_id, id_location, id_signal_par, id_detected, id_class_det from SignalEvent";
-            //EPStatement statement = engine.EPAdministrator.CreateEPL(epl);
-            //statement.Events += DefaultUpdateEventHandler;
             
             services.AddSingleton(engine);
             services.AddControllers();
         }
 
-
-        private static void DefaultUpdateEventHandler(object sender, UpdateEventArgs e)
-        {
-            var attributes = (e.NewEvents.FirstOrDefault().Underlying as Dictionary<string, object>);
-            Console.WriteLine("An Event (" + e.Statement.Name + ") occured:");
-            foreach (var att in attributes)
-            {
-                Console.WriteLine($"\t{att.Key}: {att.Value}");
-            }
-        }
 
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
