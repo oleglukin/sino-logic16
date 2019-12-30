6c227ae0248188060295a23247a594993daf89c0
Oleg Lukin <oleglukin@yahoo.com>
2019-12-30 23:06:07 +1000

    Added SignalEventAggregation

--- net/API/Models/SignalEvent.cs
+++ net/API/Models/SignalEvent.cs
@@ -3,11 +3,11 @@
     public class SignalEvent
     {
         // TODO follow naming conventions
-        public string id_sample { get; set; } // идетнификатор предмета(76rtw)
-        public string num_id { get; set; } // серийный номер предмета(ffg#er111)
-        public string id_location { get; set; } // место откуда(3211.2334), а может быть(Екатеринбург)
-        public string id_signal_par { get; set; } // датчик, с которого идет сигнал(0xcv11cs)
-        public string id_detected { get; set; } // данные о состоянии(None), - исправен, (Nan), - поломка
-        public string id_class_det { get; set; } // вид поломки(req11)
+        public string id_sample { get; set; } // item identifier (76rtw)
+        public string num_id { get; set; } // item serial number (ffg#er111)
+        public string id_location { get; set; } // location from (3211.2334), can be a name
+        public string id_signal_par { get; set; } // sensor generating signal (0xcv11cs)
+        public string id_detected { get; set; } // status data (None), - functional, (Nan), - failed
+        public string id_class_det { get; set; } // failure type (req11)
     }
 }
