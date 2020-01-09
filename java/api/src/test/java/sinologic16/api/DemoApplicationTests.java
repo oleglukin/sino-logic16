40b98151be729a3ffb6569f55967d1c59e04c5cf
Oleg Lukin <oleglukin@yahoo.com>
2020-01-09 23:17:59 +1000

    Added blank java project

---
+++ java/api/src/test/java/sinologic16/api/DemoApplicationTests.java
@@ -0,0 +1,13 @@
+package sinologic16.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
