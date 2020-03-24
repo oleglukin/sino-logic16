40a10d59dfea7a827f25ba79609c7ddf424d9f47
Oleg Lukin <oleg.e.lukin@hotmail.com>
2020-03-24 17:30:32 +0300

    Moved controller to another folder

--- java/api/src/main/java/sinologic16/api/controller/SignalEventController.java
+++ java/api/src/main/java/sinologic16/api/controller/SignalEventController.java
@@ -1,4 +1,4 @@
-package sinologic16.api;
+package sinologic16.api.controller;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
